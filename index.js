010999628af7241da8a17f94ce0134e70a8cb082
Alan Gutierrez <alan@prettyrobots.com>
2012-12-02 03:10:35 -0500

    Serve a file from the NODE_PATH.
    Implemented the Connect NPM function builder function.
    
    Implemented a test using `request` to the Connect NPM middle.
    
    Discovered that `require.resolve` does not work with Node.js core
    modules.

---
+++ index.js
@@ -0,0 +1,27 @@
+var send = require("send"),
+    util = require("util"),
+    parse = require("url").parse,
+    normalize = require("path").normalize;
+
+// The module consists of a single function.
+module.exports = function (options) {
+  // Set default format and a default of no modules.
+  var format = options.format || "/npm/%s",
+      modules = options.modules || {},
+      actual = {};
+  // This will raise an exception with an error code of `MODULE_NOT_FOUND` if
+  // the module cannot be resolved.
+  modules.forEach(function (module) {
+    actual[util.format(format, module)] = require.resolve(module);
+  });
+  // If the URL is requesting a module we've been permitted to send, send it,
+  // otherwise move onto the next handler.
+  return function (req, res, next) {
+    var url = parse(req.url), path = normalize(url.pathname), module;
+    if (module = actual[path]) {
+      send(req, module).pipe(res);
+    } else {
+      next();
+    }
+  }
+}
